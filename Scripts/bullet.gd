418fd41f3d057dba9dfc98dd7b07eb282e771c62
oli <Assassinbid225@gmail.com>
2026-06-03 17:19:38 +0100

    Update bullet.tscn & bullet.gd
    Changed signal type on bullet

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -9,9 +9,9 @@ func _process(delta: float) -> void:
 	position += transform.x * SPEED * delta
 
 func _on_visible_on_screen_enabler_2d_screen_exited() -> void:
-	queue_free() # Replace with function body.
+	queue_free()
 
-
-
-func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
-	queue_free() # Replace with function body.
+func _on_body_entered(body: Node2D) -> void:
+	queue_free()
+	if body.has_method("take_damage"):
+		body.take_damage()

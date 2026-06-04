6b224480edc47eb4e6358ae04b1069014b151722
oli <5048811+olzorz@users.noreply.github.com>
2026-06-04 20:28:27 +0100

    State Machine target timer update
    Target timer now handled within the script as opposed to timer

--- Scripts/police.gd
+++ Scripts/police.gd
@@ -5,7 +5,8 @@ var hp : int = 2
 @export var movement_speed : float = 100.0
 var player: Node2D
 var direction : Vector2
-var movement_timer : bool = false
+@export var update_cooldown : float = 0.5
+var update_time : float = update_cooldown
 var target_location : Vector2 = Vector2.ZERO
 
 enum State {
@@ -21,20 +22,23 @@ func _ready():
 
 func _physics_process(delta: float) -> void:
 	if player:
-		target_location = player.global_position
+		update_time -= delta
+		if update_time <= 0:
+			target_location = player.global_position
+			direction = target_location - global_position
+			update_time = update_cooldown
+		look_at(player.global_position)
 	else:
 		state = State.IDLE
+	
 	match state:
 		State.CHASE:
-			if movement_timer:
-				direction = target_location - global_position
+			
 			if direction.length() > 100:
 				velocity = direction.normalized() * movement_speed
 			else:
 				state = State.SHOOT
-			movement_timer = false
-			
-			
+		
 		State.IDLE:
 			if player:
 				state = State.CHASE
@@ -42,7 +46,7 @@ func _physics_process(delta: float) -> void:
 				velocity = velocity.move_toward(Vector2.ZERO, movement_speed)
 		
 		State.SHOOT:
-			if player and (target_location - global_position).length() > 150:
+			if player and direction.length() > 150:
 				state = State.CHASE
 			elif not player:
 				state = State.IDLE
@@ -50,13 +54,9 @@ func _physics_process(delta: float) -> void:
 				velocity = velocity.move_toward(Vector2.ZERO, movement_speed)
 
 
-	look_at(target_location)
+	
 	move_and_slide()
 
 
 func is_dead() -> void:
 	queue_free()
-
-
-func movement_calculation() -> void:
-	movement_timer = true

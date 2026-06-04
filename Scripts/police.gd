61c91bfa2210e95298fb8d4aa80728e9d30883a9
oli <Assassinbid225@gmail.com>
2026-06-04 18:35:55 +0100

    Update police.gd
    Quite buggy police state machine.
    Switch between IDLE, SHOOT, CHASE states.
    
    IDLE for dead player
    SHOOT for in-range police
    CHASE for out-of-range police

--- Scripts/police.gd
+++ Scripts/police.gd
@@ -8,20 +8,48 @@ var direction : Vector2
 var movement_timer : bool = false
 var target_location : Vector2 = Vector2.ZERO
 
+enum State {
+	CHASE,
+	SHOOT,
+	IDLE
+}
+var state := State.CHASE
 
 func _ready():
 	player = $"../player"
+	
 
 func _physics_process(delta: float) -> void:
-	if player:
-		target_location = player.global_position
-	if movement_timer:
-		direction = target_location - global_position
-		if direction.length() > 100:
-			velocity = direction.normalized() * movement_speed
-		else:
-			velocity = velocity.move_toward(Vector2.ZERO, movement_speed)
-		movement_timer = false
+	match state:
+		State.CHASE:
+			if player:
+				target_location = player.global_position
+			else:
+				state = State.IDLE
+			
+			if movement_timer:
+				direction = target_location - global_position
+			if direction.length() > 100:
+				velocity = direction.normalized() * movement_speed
+			else:
+				state = State.SHOOT
+			movement_timer = false
+			
+		State.IDLE:
+			if player:
+				state = State.CHASE
+			else:
+				velocity = velocity.move_toward(Vector2.ZERO, movement_speed)
+		
+		State.SHOOT:
+			if player and (target_location - player.global_position).length() > 150:
+				state = State.CHASE
+			elif not player:
+				state = State.IDLE
+			else:
+				velocity = velocity.move_toward(Vector2.ZERO, movement_speed)
+
+
 	look_at(target_location)
 	move_and_slide()
 

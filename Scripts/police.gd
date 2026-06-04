1dfae23b71c78e72e54f556c650ac6e552bffe6b
oli <5048811+olzorz@users.noreply.github.com>
2026-06-04 16:53:57 +0100

    Added Enemy Movement Timer
    Police now calculate their movements every 0.5s, removing the jank when at the edge of their range around the player.

--- Scripts/police.gd
+++ Scripts/police.gd
@@ -2,22 +2,29 @@ extends CharacterBody2D
 
 
 var hp : int = 2
-const SPEED = 50.0
+@export var movement_speed : float = 100.0
 var player: Node2D
 var direction : Vector2
+var movement_timer : bool = false
 
 
 func _ready():
 	player = $"../player"
 
 func _physics_process(delta: float) -> void:
-	direction = player.global_position - global_position
-	if direction.length() > 100:
-		velocity = direction.normalized() * SPEED
-	else:
-		velocity = Vector2.ZERO
+	if movement_timer:
+		direction = player.global_position - global_position
+		if direction.length() > 100:
+			velocity = direction.normalized() * movement_speed
+		else:
+			velocity = velocity.move_toward(Vector2.ZERO, movement_speed)
+		movement_timer = false
 	look_at(player.global_position)
 	move_and_slide()
 
 func is_dead() -> void:
 	queue_free()
+
+
+func movement_calculation() -> void:
+	movement_timer = true # Replace with function body.

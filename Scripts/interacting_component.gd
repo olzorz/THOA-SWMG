12d78c21f9720d2921b15aaebeee4da18c727583
oli <5048811+olzorz@users.noreply.github.com>
2026-06-07 00:55:34 +0100

    Update interacting_component.gd
    Bugfix

--- Scripts/interacting_component.gd
+++ Scripts/interacting_component.gd
@@ -8,12 +8,12 @@ var can_interact : bool = true
 @onready var label_initial_position: Vector2 = get_parent().global_position - interact_label.global_position
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("interact"):
+	if event.is_action_pressed("interact") and can_interact:
 		if current_interactions:
 			can_interact = false
 			interact_label.hide()
-			
-			await current_interactions[0].interact.call()
+			if current_interactions[0].is_interactable:
+				await current_interactions[0].interact.call()
 			
 			can_interact = true
 
@@ -28,8 +28,8 @@ func _process(_delta: float) -> void:
 		interact_label.hide()
 
 func _sort_by_nearest(area1, area2):
-	var area1_dist = global_position.distance_to(area1)
-	var area2_dist = global_position.distance_to(area2)
+	var area1_dist = global_position.distance_to(area1.global_position)
+	var area2_dist = global_position.distance_to(area2.global_position)
 	return area1_dist < area2_dist
 
 func _on_interact_range_area_entered(area: Area2D) -> void:

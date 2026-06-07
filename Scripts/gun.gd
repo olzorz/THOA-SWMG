d5b4373e78df936c8bcff3607014733b2356c635
oli <me@opth.uk>
2026-06-07 16:05:11 +0100

    Added muzzle flash

--- Scripts/gun.gd
+++ Scripts/gun.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 const BULLET := preload("res://Scenes/bullet.tscn")
 @export var player : bool = false
+@onready var muzzle_flash: GPUParticles2D = $MuzzleFlash
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("shoot") and player:
@@ -12,3 +13,4 @@ func shoot() -> void:
 	get_tree().root.add_child(bullet_instance)
 	bullet_instance.global_position = global_position
 	bullet_instance.global_rotation = global_rotation
+	muzzle_flash.restart()

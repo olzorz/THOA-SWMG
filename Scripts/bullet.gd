2c9c01eed4ed675e42e6d0f89e14a9eb2d30e6bb
oli <me@opth.uk>
2026-06-10 18:31:19 +0100

    Removed redundant bullet.gd variable
    Can use sprite visibility instead of a dedicated variable.

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -5,7 +5,6 @@ extends Area2D
 @onready var bullet_hit: GPUParticles2D = $BulletHit
 @onready var sprite: Sprite2D = $Sprite2D
 
-var bullet_existence : bool = true
 
 func _process(delta: float) -> void:
 	position += transform.x * speed * delta
@@ -14,9 +13,8 @@ func _on_visible_on_screen_enabler_2d_screen_exited() -> void:
 	queue_free()
 
 func _on_body_entered(body: Node2D) -> void:
-	if bullet_existence == false:
+	if not sprite.visible:
 		return
-	bullet_existence = false
 	var health_component := body.get_node_or_null("HealthComponent") as HealthComponent
 	if health_component:
 		health_component.take_damage(damage)
